62b3218730030ab206b4ff27b7a68fc3099f1c38
tolgat <Maya129903>
2016-04-25 16:04:46 +0300

    init

--- src/test/java/com/tt/testautomation/BasicTestWD.java
+++ src/test/java/com/tt/testautomation/BasicTestWD.java
@@ -1,48 +1,45 @@
 package com.tt.testautomation;
 /**
  * Created by tolga on 22/04/16.
- *
+ * <p>
  * tt
  */
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
-public class BasicTest {
+public class BasicTestWD extends DriverFactory {
 
     private void googleExampleThatSearchesFor(final String
-                                                      searchString) {
-        WebDriver driver = new FirefoxDriver();
+                                                      searchString) throws Exception {
+        WebDriver driver = DriverFactory.getDriver();
         driver.get("http://www.google.com");
         WebElement searchField = driver.findElement(By.name("q"));
         searchField.clear();
-
         searchField.sendKeys(searchString);
         System.out.println("Page title is: " + driver.getTitle());
         searchField.submit();
         (new WebDriverWait(driver, 10)).until(new
                                                       ExpectedCondition<Boolean>() {
                                                           public Boolean apply(WebDriver driverObject) {
-                                                              return driverObject.getTitle().toLowerCase()
-                                                                      .startsWith(searchString.toLowerCase());
+                                                              return driverObject.getTitle().toLowerCase().
+                                                                      startsWith(searchString.toLowerCase());
                                                           }
                                                       });
         System.out.println("Page title is: " + driver.getTitle());
-        driver.quit();
     }
 
     @Test
-    public void googleCheeseExample() {
+    public void googleCheeseExample() throws Exception {
         googleExampleThatSearchesFor("Cheese!");
     }
 
     @Test
-    public void googleMilkExample() {
+    public void googleMilkExample() throws Exception {
         googleExampleThatSearchesFor("Milk!");
     }
 
